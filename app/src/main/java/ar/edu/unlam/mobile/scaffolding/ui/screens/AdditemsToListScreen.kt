161f6b6f693ea3f0b40f711267703996527b24ab
Leandro Luna <leandro.luna200@hotmail.com>
2024-11-03 11:26:52 -0300

    format ktlin 2

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/AdditemsToListScreen.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/AdditemsToListScreen.kt
@@ -112,28 +112,27 @@ fun CategoryItem(
     Card(modifier = modifier) {
         Column(
             modifier =
-            Modifier
-                .animateContentSize(
-                    animationSpec =
-                    spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMedium,
-                    ),
-                )
-                .background(color = color),
+                Modifier
+                    .animateContentSize(
+                        animationSpec =
+                            spring(
+                                dampingRatio = Spring.DampingRatioNoBouncy,
+                                stiffness = Spring.StiffnessMedium,
+                            ),
+                    ).background(color = color),
         ) {
             Row(
                 modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp),
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
             ) {
                 NameCategory(
                     nameCategory = category.name,
                     modifier =
-                    Modifier
-                        .fillMaxWidth()
-                        .weight(1f),
+                        Modifier
+                            .fillMaxWidth()
+                            .weight(1f),
                 )
 
                 // Spacer(modifier = Modifier.weight(1f))
@@ -144,9 +143,9 @@ fun CategoryItem(
                 ItemsListBody(
                     items = category.listItem,
                     modifier =
-                    Modifier.padding(
-                        4.dp,
-                    ),
+                        Modifier.padding(
+                            4.dp,
+                        ),
                     checkedStates = checkedStates,
                     onItemCheckedChange = onItemCheckedChange,
                 )
@@ -185,9 +184,9 @@ private fun ItemRow(
 ) {
     Row(
         modifier =
-        Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
+            Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceAround,
     ) {
