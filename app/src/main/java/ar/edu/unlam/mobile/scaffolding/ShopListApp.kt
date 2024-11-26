fe27ad245c49c04b8bd15eaca0fb2e952c3a46bf
Federico Navarro <navarrofederico14@gmail.com>
2024-11-26 10:30:29 -0300

    Se comentó implementación vieja de navegación

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ShopListApp.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ShopListApp.kt
@@ -1,49 +1,23 @@
 package ar.edu.unlam.mobile.scaffolding
 
-import android.util.Log
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.Divider
-import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import ar.edu.unlam.mobile.scaffolding.ui.components.BottomBar
 import ar.edu.unlam.mobile.scaffolding.ui.navigation.AppNavHost
-import ar.edu.unlam.mobile.scaffolding.ui.navigation.AppScreens
-import ar.edu.unlam.mobile.scaffolding.ui.screens.AdditemsDestination
-import ar.edu.unlam.mobile.scaffolding.ui.screens.HomeDestination
-import ar.edu.unlam.mobile.scaffolding.ui.screens.NewListDestination
-import ar.edu.unlam.mobile.scaffolding.ui.screens.ShoppingListDestination
-import kotlinx.coroutines.launch
 
 @Composable
 fun ShopListApp2(navController: NavHostController = rememberNavController()) {
@@ -99,7 +73,8 @@ fun ShopListTopAppBar(
         },
     )
 }
-
+// SE COMENTA hasta estar seguro de su eliminación
+/*
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShopListApp() {
@@ -228,3 +203,4 @@ private fun AddFAB(
         Icon(Icons.Filled.Add, contentDescription = "Add items to list")
     }
 }
+*/
